fc915915e417c84b92a232882d0528f6910c3cfd
lee-LAB <jiho264@naver.com>
2024-10-03 21:50:24 +0900

    .

--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 schemes=("Sqrt2_17" "BitLog2_Single_17" "BitLog2_Half_16" "BitLog2_Half_17")
-# 모델 리스트
 models=("deit_tiny" "deit_small" "deit_base" "vit_small" "vit_base") #"swin_small" "swin_base" "swin_tiny")
 
-# 실행할 스크립트
-
-# 모델별로 반복 실행
 for scheme in "${schemes[@]}"
 do
     for model in "${models[@]}"
